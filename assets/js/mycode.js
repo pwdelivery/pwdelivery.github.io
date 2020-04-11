f08a1b6f6b8f2ec893b1d099f15ed672f49e2962
Chris Rennie <crennie123@gmail.com>
2020-04-11 15:09:05 -0700

    push

--- assets/js/mycode.js
+++ assets/js/mycode.js
@@ -1,5 +1,10 @@
-function printFlower() {
-
+function printFlower(data) {
+    let flowerHTML;
+    for (let i=0; i<data.length; i++) {
+        flowerHTML += "<div class=\"col-3 col-6-medium col-12-small\">";
+        flowerHTML += "<section class=\"box feature\">";
+        flowerHTML += "<a class=\"image featured\"><img src=" + data[i]["images"]["feature"] + " alt=\"\" /></a>";
+    }
 }
 
 function printProduct(name) {
@@ -9,7 +14,7 @@ function printProduct(name) {
                 return response.json();
             })
             .then((data) => {
-                console.log(data);
+                printFlower(data);
             })
             .catch( (error) => {
                 console.error(error);
