48aabea7245a0aa41e5b7e780e47185f64dc2d48
Chris Rennie <crennie123@gmail.com>
2020-04-12 21:58:36 -0700

    push

--- assets/js/mycode.js
+++ assets/js/mycode.js
@@ -52,33 +52,11 @@ function customSelectMain() {
     }
   }
 
-/*
-function printFlower(data) {
-    let flowerHTML;
-    for (let i=0; i<data.length; i++) {
-        flowerHTML += "<div class=\"col-3 col-6-medium col-12-small\">";
-        flowerHTML += "<section class=\"box feature\">";
-        flowerHTML += "<a class=\"image featured\"><img src=" + data[i]["images"]["feature"] + " alt=\"\" /></a>";
-        flowerHTML += "<h3>" + data[i]["strain"] + "</h3>";
-        flowerHTML += "<div style=\"display:flex; align-items: center;\">"
-        flowerHTML += "<div class=\"custom-select\" style=\"width:200px;flex:1;\">";
-        flowerHTML += "<select><option value=\"0\">See Prices:</option>";
-        flowerHTML += "<option value=\"1\">3.5g / " + data[i]["prices"]["eigth"] + "</option>";
-        flowerHTML += "<option value=\"2\">7g / " + data[i]["prices"]["quarter"] + "</option>";
-        flowerHTML += "<option value=\"3\">14g / " + data[i]["prices"]["half"] + "</option>";
-        flowerHTML += "<option value=\"4\">28g / " + data[i]["prices"]["full"] + "</option></select></div>";
+function printFlowerOrder(data) {
+    
+}
 
-        if (data[i]["sale?"]) {
-            flowerHTML += "<h4 class=\"pricing\" style=\"flex:1;\"></a>28g/<a class=\"strike-through\">$140<a class=\"sale</a></h4>"
-        }
-
-        flowerHTML += "</div><h4 class=\"details\"><b class=\"label\">Variety: </b>" + data[i]["variety"] + "</h4>";
-
-        document.getElementById("flower-menu").innerHTML = flowerHTML;
-    }
-}*/
-
-function printFlower(data) {
+function printFlowerMenu(data) {
     let flowerHTML;
     for (let i=0; i<data.length; i++) {
         flowerHTML = "\
@@ -96,7 +74,7 @@ function printFlower(data) {
                         <option value=\"4\">28g / " + data[i]["prices"]["full"] + "</option>\
                         </select>\
                     </div>";
-       
+
         if (data[i]["sale?"]) {
             flowerHTML += "\
                     <h4 class=\"pricing\" style=\"flex:1;\"></a>28g/<a class=\"strike-through\">$140<a class=\"sale\"> $120</a></h4>";
@@ -120,17 +98,26 @@ function printFlower(data) {
     customSelectMain();
 }
 
-function printProduct(name) {
-    if (name === "flower") {
+function printData(name) {
+    if (name === "flower-menu" || name === "flower-order") {
         fetch("./data/flower.json")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                printFlower(data);
-            })
-            .catch( (error) => {
-                console.error(error);
-            });
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (name === "flower-menu") {
+                printFlowerMenu(data);
+            }
+            else if (name === "flower-order") {
+                printFlowerOrder(data);
+            }
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
     }
+}
+
+function orderForm() {
+
 }
